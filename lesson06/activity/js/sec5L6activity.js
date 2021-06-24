97ee0558dae2635a1d23f3f239248f518c8c7210
djr206 <dan.bjl@gmail.com>
2021-06-24 14:51:33 -0700

    clean activityL6, setup assignmentL6

---
+++ lesson06/activity/js/sec5L6activity.js
@@ -0,0 +1,9 @@
+{/* <input type="text" id="input2" disabled /> */}
+{/* <button id="button2">Enable Input</button> */}  
+// $("#button2").click(function () {
+//     $("#input2").prop("disabled", false);
+// });
+
+$("#button2").click(function () {
+    $("#button1").prop("disabled", false);
+});
