5e41fd85c953b265cc3dd9ba8da037375d08fdc7
djr206 <dan.bjl@gmail.com>
2021-06-25 13:36:19 -0700

    added navbar code from bootstrap.com

---
+++ lesson06/assignment/js/sec3L6assignment.js
@@ -0,0 +1,18 @@
+{/* <input type="text" id="input2" disabled /> */}
+{/* <button id="button2">Enable Input</button> */}  
+// $("#button2").click(function () {
+//     $("#input2").prop("disabled", false);
+// });
+
+$("#button2").click(function () {
+    $("#button1").prop("disabled", false);
+    $("#button1").prop("title", '');
+    $( "#button1" ).removeClass( "btn-outline-primary" ).addClass("btn-primary" );
+    $("#button1").click(function () {
+        alert('Your form has been submitted.');
+    });
+});
+// alert works outside of click function as well
+// $("#button1").click(function () {
+//     alert('Your form has been submitted.');
+// });
