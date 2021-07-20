474ff3f41b8f32d773330aea979daaf07508b127
djr206 <dan.bjl@gmail.com>
2021-07-20 08:38:10 -0700

    initial Sec3L7 commit

--- lesson07/activity06/js/sec5L6activity.js
+++
@@ -1,9 +0,0 @@
-{/* <input type="text" id="input2" disabled /> */}
-{/* <button id="button2">Enable Input</button> */}  
-// $("#button2").click(function () {
-//     $("#input2").prop("disabled", false);
-// });
-
-$("#button2").click(function () {
-    $("#button1").prop("disabled", false);
-});
